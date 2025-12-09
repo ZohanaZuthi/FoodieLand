3700409dac4a20b469b8ee984c5cce2e39ee0d4e
Nazifa Fairuz Zuthi <startrack613@gmail.com>
2025-12-09 15:59:57 +0600

    initial commit

--- frontend/src/components/layout/Navbar.jsx
+++ frontend/src/components/layout/Navbar.jsx
@@ -1,19 +1,19 @@
 import { FaFacebookF, FaTwitter, FaInstagram } from "react-icons/fa";
-
+import { HiMenu } from "react-icons/hi"; 
 export default function Navbar() {
   return (
-    <nav className="w-full bg-white border-b">
-      <div className="max-w-6xl mx-auto px-6 py-6 flex ">
+    <nav className="w-150% bg-white border-b border-gray-300 px-0">
+      <div className="max-w-6xl mx-auto px-6 pt-4 pb-1 flex items-end ">
 
         {/* LEFT — Logo */}
-        <div className="flex-auto justify-end">
+        <div className="flex flex-1  text-[16px] md:text-[24px]">
           <h1 style={{fontFamily:"Lobster"}}className="text-3xl">
             Foodieland<span className="text-red-500">.</span>
           </h1>
         </div>
 
         {/* CENTER — Menu */}
-        <div className="flex-auto flex justify-center  text-[16px] text-gray-800 gap-[20px]">
+        <div className="flex flex-auto justify-center text-[14px] md:text-[16px] gap-[30px] md:gap-[60px]  text-gray-800">
           <span className="cursor-pointer hover:text-black">Home</span>
           <span className="cursor-pointer hover:text-black">Recipes</span>
           <span className="cursor-pointer hover:text-black">Blog</span>
@@ -22,7 +22,7 @@ export default function Navbar() {
         </div>
 
         {/* RIGHT — Social Icons */}
-        <div className="flex-auto flex justify-end gap-6 text-xl text-gray-800">
+        <div className="hidden md:flex flex-auto justify-end gap-6 text-xl text-gray-800 ">
           <FaFacebookF className="cursor-pointer hover:text-black" />
           <FaTwitter className="cursor-pointer hover:text-black" />
           <FaInstagram className="cursor-pointer hover:text-black" />
