f70ef99d3f0ad15e820aa49258790b35e432204f
Nazifa Fairuz Zuthi <startrack613@gmail.com>
2025-12-11 21:04:46 +0600

    Rest_framework

--- backend/recipes/serializers.py
+++ backend/recipes/serializers.py
@@ -4,3 +4,51 @@ from .models import (
     IngredientGroup, Ingredient, Instruction
 )
 
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = "__all__"
+
+
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Author
+        fields = "__all__"
+
+
+class NutritionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Nutrition
+        fields = "__all__"
+
+
+class IngredientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ingredient
+        fields = ["name"]
+
+
+class IngredientGroupSerializer(serializers.ModelSerializer):
+    items = IngredientSerializer(many=True)
+
+    class Meta:
+        model = IngredientGroup
+        fields = ["group_name", "items"]
+
+
+class InstructionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Instruction
+        fields = ["step_number", "text", "image"]
+
+
+class RecipeSerializer(serializers.ModelSerializer):
+    nutrition = NutritionSerializer()
+    ingredient_groups = IngredientGroupSerializer(many=True)
+    instructions = InstructionSerializer(many=True)
+    author = AuthorSerializer()
+    category = CategorySerializer()
+
+    class Meta:
+        model = Recipe
+        fields = "__all__"
